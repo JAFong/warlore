692588649fe4069aeff6d1aed5d5a5e1e0a1200f
Justin Fong <jimmyfong@Justins-Macbook.local>
2016-01-06 12:05:35 -0800

    added css styling and animations for header sliding

--- app/js/header/headerBox.jsx
+++ app/js/header/headerBox.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 class HeaderBox extends React.Component {
   render() {
-    return <div className="header-box">I am a header box!</div>
+    return (
+      <div className="header-wrapper">
+        <div className="header-box">I am a header box!</div>
+      </div>
+    )
   }
 }
 
